18cffb0124183fa3dfb6e4649813382107de488e
Drew Williams <williams.r.drew@gmail.com>
2024-05-22 13:59:18 -0400

    added entt example code

--- src/game_backend/main.cpp
+++ src/game_backend/main.cpp
@@ -1,13 +1,21 @@
 #include <CLI/CLI.hpp>
+#include <entt/entity/fwd.hpp>
 #include <spdlog/spdlog.h>
 #include <cstdlib>
 #include <exception>
 #include <fmt/core.h>
+#include <entt/entity/registry.hpp>
 
 // This file will be generated automatically when cur_you run the CMake
 // configuration step. It creates a namespace called `tdgame`. You can modify
 // the source template at `configured_files/config.hpp.in`.
 #include <internal_use_only/config.hpp>
+
+
+struct position {
+  double x;
+  double y;
+};
 
 // NOLINTNEXTLINE(bugprone-exception-escape)
 int main(int argc, const char **argv)
@@ -29,5 +37,20 @@ int main(int argc, const char **argv)
 
   } catch (const std::exception &e) {
     spdlog::error("Unhandled exception in main: {}", e.what());
+  }
+
+  entt::basic_registry registry;
+
+  const int numberOfEntities = 10;
+  const double xPosition = 20.53;
+  const double yPosition = 59.40;
+  for(int i = 0; i < numberOfEntities; i++) {
+    const auto entity = registry.create();
+
+    position pos{};
+    pos.x = xPosition;
+    pos.y = yPosition;
+
+    registry.emplace<position>(entity, pos);
   }
 }
